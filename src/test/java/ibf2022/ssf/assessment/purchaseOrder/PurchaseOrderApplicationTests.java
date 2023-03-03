50f3a156aad2b0f44e77bdf370b49f7b2d2918b5
Gerald Ng Qi Long <ngqilong@gmail.com>
2023-03-03 09:22:26 +0800

    initial commit

---
+++ src/test/java/ibf2022/ssf/assessment/purchaseOrder/PurchaseOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf2022.ssf.assessment.purchaseOrder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
